970cad8c74f92ebe496225a63e25aebc40eb8e12
Gaspard Hedde <gaspard_hedde@yahoo.com>
2026-01-06 14:53:26 +0100

    FIX: WIP second version

---
+++ src/main.js
@@ -0,0 +1,230 @@
+import './styles.css'
+import gsap from 'gsap';
+
+const items = gsap.utils.toArray('.project');
+const count = items.length;
+
+function calculateRadius() {
+  if (window.innerWidth < 768) {
+    return {
+      x: window.innerWidth * 1.2,
+      y: window.innerHeight * 0.5
+    };
+  } else {
+    return {
+      x: window.innerWidth * 0.8,
+      y: window.innerHeight * 0.7
+    };
+  }
+}
+
+let radiusX = calculateRadius().x;
+let radiusY = calculateRadius().y;
+
+const scroll = {
+  current: 0,
+  target: 0,
+  ease: 0.05,
+};
+
+const drag = {
+  active: false,
+  startY: 0,
+  startScroll: 0
+};
+
+const angles = items.map((_, i) => (i / count) * Math.PI * 2);
+
+//wheel
+window.addEventListener('wheel', (e) => {
+  const delta = gsap.utils.clamp(-0.08, 0.08, e.deltaY * 0.0015);
+  scroll.target += delta;
+});
+
+
+//touch
+window.addEventListener('touchstart', (e) => {
+  drag.active = true;
+  drag.startY = e.touches[0].clientY;
+  drag.startScroll = scroll.target;
+});
+
+window.addEventListener('touchmove', (e) => {
+  if (!drag.active) return;
+  const deltaY = e.touches[0].clientY - drag.startY;
+  scroll.target = drag.startScroll - deltaY * 0.005;
+});
+
+window.addEventListener('touchend', () => {
+  drag.active = false;
+});
+
+
+function initializePositions() {
+  items.forEach((el, i) => {
+    const a = angles[i];
+    gsap.set(el, {
+      x: Math.cos(a) * radiusX,
+      y: window.innerHeight / 2 - el.offsetHeight / 2 + Math.sin(a) * radiusY,
+      rotate: 0,
+      zIndex: count - i
+    });
+  });
+}
+
+window.addEventListener('load', () => {
+  setTimeout(initializePositions, 100);
+});
+
+// Resize
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 768) {
+    radiusX = window.innerWidth * 1.2;
+    radiusY = window.innerHeight * 0.5;
+  } else {
+    radiusX = window.innerWidth * 0.8;
+    radiusY = window.innerHeight * 0.7;
+  }
+  
+  items.forEach((el, i) => {
+    if (el.classList.contains('active')) return;
+    const a = angles[i] + scroll.current;
+    gsap.set(el, {
+      x: Math.cos(a) * radiusX,
+      y: window.innerHeight / 2 - el.offsetHeight / 2 + Math.sin(a) * radiusY
+
+    });
+  });
+});
+
+
+gsap.ticker.add(() => {
+  scroll.current += (scroll.target - scroll.current) * scroll.ease;
+  items.forEach((el, i) => {
+    if (el.classList.contains('active')) return;
+    const a = angles[i] + scroll.current;
+    gsap.set(el, {
+      x: Math.cos(a) * radiusX,
+      y: window.innerHeight / 2 - el.offsetHeight / 2 + Math.sin(a) * radiusY
+
+    });
+  });
+});
+
+
+
+const projects = document.querySelectorAll('.project');
+const activeBackground = document.querySelector('.active-background');
+const activeProject = document.getElementById('project-background');
+
+const suptitle = document.getElementById('suptitle');
+const name = document.getElementById('name');
+const description = document.getElementById('description');
+
+const navigation = document.querySelector('.navigation');
+
+// open project
+projects.forEach((project, index) => {
+  project.addEventListener('click', () => {
+
+    if (window.innerWidth < 768) {
+      gsap.to(navigation, {
+        opacity: 0,
+        duration: 0.7,
+        ease: 'power2.inOut',
+      });
+    }
+
+    const img = project.querySelector('img');
+    const bgSrc = img.getAttribute('data-bg-src');
+    activeProject.src = bgSrc;
+
+    const projectSuptitle = project.querySelector('.suptitle');
+    const suptitleContent = projectSuptitle.getAttribute('data-content');
+    const projectName = project.querySelector('.name');
+    const nameContent = projectName.getAttribute('data-content');
+    const projectDescription = project.querySelector('.description');
+    const descriptionContent = projectDescription.getAttribute('data-content');
+
+    suptitle.textContent = suptitleContent;
+    name.textContent = nameContent;
+    description.textContent = descriptionContent;
+    
+
+    projects.forEach((p, i) => {
+      if (p !== project && p.classList.contains('active')) {
+        const a = angles[i] + scroll.current;
+        gsap.to(p, {
+          x: Math.cos(a) * radiusX,
+          y: Math.sin(a) * radiusY, 
+          scale: 1,
+          duration: 0.7,
+          ease: 'power2.inOut',
+          onComplete: () => {
+            p.classList.remove('active');
+          }
+        });
+      }
+    });
+    
+    if (!project.classList.contains('active')) {
+      project.classList.add('active');
+      
+      const carouselLeft = window.innerWidth < 768 ? window.innerWidth * 1.5 : window.innerWidth;
+      const carouselCenter = -carouselLeft + (window.innerWidth / 2);
+      
+      gsap.to(project, {
+        x: carouselCenter,
+        y: window.innerHeight / 2 - project.offsetHeight / 2,
+        duration: 0.7,
+        scale: 1.3, 
+        ease: 'power2.inOut',
+      });
+
+      gsap.to(activeBackground, { 
+        opacity: 1,
+        duration: 0.7,
+        ease: 'power2.inOut',
+        onComplete: () => {
+          activeBackground.style.pointerEvents = 'auto';
+        }
+      });
+    }
+  });
+});
+
+// close project
+activeBackground.addEventListener('click', () => {
+  if (window.innerWidth < 768) {
+    gsap.to(navigation, {
+      opacity: 1,
+      duration: 0.7,
+      ease: 'power2.inOut',
+    });
+  }
+
+  projects.forEach((p, i) => {
+    if (p.classList.contains('active')) {
+      const a = angles[i] + scroll.current;
+      gsap.to(p, {
+        x: Math.cos(a) * radiusX,
+        y: window.innerHeight / 2 - p.offsetHeight / 2 + Math.sin(a) * radiusY,
+        scale: 1,
+        duration: 0.7,
+        ease: 'power2.inOut',
+        onComplete: () => {
+          p.classList.remove('active');
+        }
+      });
+    }
+  });
+
+  gsap.to(activeBackground, {
+    opacity: 0,
+    duration: 0.7,
+    ease: 'power2.inOut',
+    onComplete: () => {
+      activeBackground.style.pointerEvents = 'none';
+    }
+  });
+});
--- src/mainsecond.js
+++ src/mainsecond.js
@@ -1,7 +1,9 @@
-import './styleSimple.css'
+import './secondstyles.css'
 import gsap from 'gsap';
 
 const items = gsap.utils.toArray('.project');
+const items2 = gsap.utils.toArray('.project2');
+
 const count = items.length;
 
 function calculateRadius() {
@@ -27,51 +29,32 @@ const scroll = {
   ease: 0.05,
 };
 
-
 const drag = {
   active: false,
   startY: 0,
   startScroll: 0
 };
 
-
 const angles = items.map((_, i) => (i / count) * Math.PI * 2);
 
+//wheel
 window.addEventListener('wheel', (e) => {
   const delta = gsap.utils.clamp(-0.08, 0.08, e.deltaY * 0.0015);
   scroll.target += delta;
 });
 
 
-
-window.addEventListener('mousedown', (e) => {
-  drag.active = true;
-  drag.startY = e.clientY;
-  drag.startScroll = scroll.target;
-});
-
-window.addEventListener('mousemove', (e) => {
-  if (!drag.active) return;
-  
-  const deltaY = e.clientY - drag.startY;
-  scroll.target = drag.startScroll - deltaY * 0.002;
-});
-
-window.addEventListener('mouseup', () => {
-  drag.active = false;
-});
-
-
+//touch
 window.addEventListener('touchstart', (e) => {
   drag.active = true;
   drag.startY = e.touches[0].clientY;
   drag.startScroll = scroll.target;
 });
+
 window.addEventListener('touchmove', (e) => {
   if (!drag.active) return;
-  
   const deltaY = e.touches[0].clientY - drag.startY;
-  scroll.target = drag.startScroll - deltaY * 0.002;
+  scroll.target = drag.startScroll - deltaY * 0.005;
 });
 
 window.addEventListener('touchend', () => {
@@ -81,6 +64,16 @@ window.addEventListener('touchend', () => {
 
 function initializePositions() {
   items.forEach((el, i) => {
+    const a = angles[i];
+    gsap.set(el, {
+      x: Math.cos(a) * radiusX,
+      y: window.innerHeight / 2 - el.offsetHeight / 2 + Math.sin(a) * radiusY,
+      rotate: 0,
+      zIndex: count - i
+    });
+  });
+
+  items2.forEach((el, i) => {
     const a = angles[i];
     gsap.set(el, {
       x: Math.cos(a) * radiusX,
@@ -114,6 +107,16 @@ window.addEventListener('resize', () => {
 
     });
   });
+
+  items2.forEach((el, i) => {
+    if (el.classList.contains('active')) return;
+    const a = angles[i] + scroll.current;
+    gsap.set(el, {
+      x: Math.cos(a) * radiusX,
+      y: window.innerHeight / 2 - el.offsetHeight / 2 + Math.sin(a) * radiusY
+
+    });
+  });
 });
 
 
@@ -128,23 +131,48 @@ gsap.ticker.add(() => {
 
     });
   });
+
+  items2.forEach((el, i) => {
+    if (el.classList.contains('active')) return;
+    const a = angles[i] + scroll.current;
+    gsap.set(el, {
+      x: Math.cos(a) * radiusX,
+      y: window.innerHeight / 2 - el.offsetHeight / 2 + Math.sin(a) * radiusY
+
+    });
+  });
 });
 
 
 
 const projects = document.querySelectorAll('.project');
+const projects2 = document.querySelectorAll('.project2');
+
 const activeBackground = document.querySelector('.active-background');
+const activeBackground2 = document.querySelector('.active-background2');
 const activeProject = document.getElementById('project-background');
+const activeProject2 = document.getElementById('project-background2');
+
 
 const suptitle = document.getElementById('suptitle');
 const name = document.getElementById('name');
 const description = document.getElementById('description');
-
+const suptitle2 = document.getElementById('suptitle2');
+const name2 = document.getElementById('name2');
+const description2 = document.getElementById('description2');
 
 const navigation = document.querySelector('.navigation');
 
+// open project
 projects.forEach((project, index) => {
   project.addEventListener('click', () => {
+
+    gsap.to(projects2, {
+      opacity: 0,
+      duration: 0.7,
+      ease: 'power2.inOut',
+    });
+
 
     if (window.innerWidth < 768) {
       gsap.to(navigation, {
@@ -154,7 +182,6 @@ projects.forEach((project, index) => {
       });
     }
 
-    
     const img = project.querySelector('img');
     const bgSrc = img.getAttribute('data-bg-src');
     activeProject.src = bgSrc;
@@ -190,11 +217,11 @@ projects.forEach((project, index) => {
     if (!project.classList.contains('active')) {
       project.classList.add('active');
       
-      const carouselLeft = window.innerWidth < 768 ? window.innerWidth * 1.5 : window.innerWidth;
-      const carouselCenter = -carouselLeft + (window.innerWidth / 2);
+      const projectRect = project.getBoundingClientRect();
+      const xCenter = (window.innerWidth / 2) - (projectRect.left + projectRect.width / 2);
       
       gsap.to(project, {
-        x: carouselCenter,
+        x: `+=${xCenter}`,
         y: window.innerHeight / 2 - project.offsetHeight / 2,
         duration: 0.7,
         scale: 1.3, 
@@ -207,22 +234,96 @@ projects.forEach((project, index) => {
         ease: 'power2.inOut',
         onComplete: () => {
           activeBackground.style.pointerEvents = 'auto';
-          // activeBackground.style.cursor = 'pointer';
         }
       });
     }
   });
 });
 
-activeBackground.addEventListener('click', () => {
+projects2.forEach((project, index) => {
+  project.addEventListener('click', () => {
 
+    if (window.innerWidth < 768) {
+      gsap.to(navigation, {
+        opacity: 0,
+        duration: 0.7,
+        ease: 'power2.inOut',
+      });
+    }
+
+    const img = project.querySelector('img');
+    const bgSrc = img.getAttribute('data-bg-src');
+    activeProject2.src = bgSrc;
+
+    const projectSuptitle = project.querySelector('.suptitle');
+    const suptitleContent = projectSuptitle.getAttribute('data-content');
+    const projectName = project.querySelector('.name');
+    const nameContent = projectName.getAttribute('data-content');
+    const projectDescription = project.querySelector('.description');
+    const descriptionContent = projectDescription.getAttribute('data-content');
+
+    suptitle2.textContent = suptitleContent;
+    name2.textContent = nameContent;
+    description2.textContent = descriptionContent;
+    
+
+    projects.forEach((p, i) => {
+      if (p !== project && p.classList.contains('active')) {
+        const a = angles[i] + scroll.current;
+        gsap.to(p, {
+          x: Math.cos(a) * radiusX,
+          y: Math.sin(a) * radiusY, 
+          scale: 1,
+          duration: 0.7,
+          ease: 'power2.inOut',
+          onComplete: () => {
+            p.classList.remove('active');
+          }
+        });
+      }
+    });
+    
+    if (!project.classList.contains('active')) {
+      project.classList.add('active');
+      
+      const projectRect = project.getBoundingClientRect();
+      const xCenter = (window.innerWidth / 2) - (projectRect.left + projectRect.width / 2);
+      
+      gsap.to(project, {
+        x: `+=${xCenter}`,
+        y: window.innerHeight / 2 - project.offsetHeight / 2,
+        duration: 0.7,
+        scale: 1.3, 
+        ease: 'power2.inOut',
+      });
+
+      gsap.to(activeBackground2, { 
+        opacity: 1,
+        duration: 0.7,
+        ease: 'power2.inOut',
+        onComplete: () => {
+          activeBackground2.style.pointerEvents = 'auto';
+        }
+      });
+    }
+  });
+});
+
+// close project
+activeBackground.addEventListener('click', () => {
   if (window.innerWidth < 768) {
     gsap.to(navigation, {
       opacity: 1,
-      duration: 0.,
+      duration: 0.7,
       ease: 'power2.inOut',
     });
   }
+
+  gsap.to(projects2, {
+    opacity: 1,
+    duration: 0.7,
+    ease: 'power2.inOut',
+  });
 
   projects.forEach((p, i) => {
     if (p.classList.contains('active')) {
@@ -230,7 +331,6 @@ activeBackground.addEventListener('click', () => {
       gsap.to(p, {
         x: Math.cos(a) * radiusX,
         y: window.innerHeight / 2 - p.offsetHeight / 2 + Math.sin(a) * radiusY,
-
         scale: 1,
         duration: 0.7,
         ease: 'power2.inOut',
@@ -247,6 +347,42 @@ activeBackground.addEventListener('click', () => {
     ease: 'power2.inOut',
     onComplete: () => {
       activeBackground.style.pointerEvents = 'none';
+    }
+  });
+});
+
+
+activeBackground2.addEventListener('click', () => {
+  if (window.innerWidth < 768) {
+    gsap.to(navigation, {
+      opacity: 1,
+      duration: 0.7,
+      ease: 'power2.inOut',
+    });
+  }
+
+  projects2.forEach((p, i) => {
+    if (p.classList.contains('active')) {
+      const a = angles[i] + scroll.current;
+      gsap.to(p, {
+        x: Math.cos(a) * radiusX,
+        y: window.innerHeight / 2 - p.offsetHeight / 2 + Math.sin(a) * radiusY,
+        scale: 1,
+        duration: 0.7,
+        ease: 'power2.inOut',
+        onComplete: () => {
+          p.classList.remove('active');
+        }
+      });
+    }
+  });
+
+  gsap.to(activeBackground2, {
+    opacity: 0,
+    duration: 0.7,
+    ease: 'power2.inOut',
+    onComplete: () => {
+      activeBackground2.style.pointerEvents = 'none';
     }
   });
 });
